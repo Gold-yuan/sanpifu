bdcd7bc28ccd264e48d451f2d066fa5ba58a7efa
yuan <515789026@qq.com>
2019-09-02 01:02:36 +0800

    html跳转

--- src/main/java/com/man/controller/PageController.java
+++ src/main/java/com/man/controller/PageController.java
@@ -2,13 +2,20 @@ package com.man.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PageController {
 
 	@GetMapping("/")
-	public String goPage() {
+	public String goHome() {
 		System.out.println(1);
-		return "index";
+		return "index.html";
+	}
+
+	@GetMapping("/{html}")
+	public String goPage(@PathVariable String html) {
+		System.out.println("html");
+		return html;
 	}
 }
